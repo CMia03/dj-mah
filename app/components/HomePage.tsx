bd56cf3d82229e54ce5a228f5cba82f6c3559bf0
CMia03 <cathyliantsoa9@gmail.com>
2026-02-25 18:51:12 +0300

    width reseau sociaux

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -298,12 +298,12 @@ export default function HomePage() {
               </p>
             </div>
           </div>
-          <div className="mt-6 flex flex-wrap gap-4 justify-center sm:justify-start">
+          <div className="mt-6 flex flex-wrap gap-4 justify-between w-full sm:flex-nowrap">
             <a
               href="https://web.facebook.com/search/top?q=rananja%20mahefa"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-3 rounded-2xl border border-white/10 bg-white/5 px-6 py-4 transition hover:-translate-y-1 hover:border-white/20 hover:bg-white/10"
+              className="flex items-center gap-3 rounded-2xl border border-white/10 bg-white/5 px-6 py-4 transition hover:-translate-y-1 hover:border-white/20 hover:bg-white/10 w-full"
             >
               <Facebook size={24} className="text-[#1877F2]" />
               <span className="font-semibold">Facebook</span>
@@ -312,7 +312,7 @@ export default function HomePage() {
               href="https://www.instagram.com/rananja_mahefa/"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-3 rounded-2xl border border-white/10 bg-white/5 px-6 py-4 transition hover:-translate-y-1 hover:border-white/20 hover:bg-white/10"
+              className="flex items-center gap-3 rounded-2xl border border-white/10 bg-white/5 px-6 py-4 transition hover:-translate-y-1 hover:border-white/20 hover:bg-white/10 w-full"
             >
               <Instagram size={24} className="text-[#E4405F]" />
               <span className="font-semibold">Instagram</span>
@@ -321,7 +321,7 @@ export default function HomePage() {
               href="https://www.tiktok.com/@rananja.m?_r=1&_t=ZS-94DcPISxr5Z"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-3 rounded-2xl border border-white/10 bg-white/5 px-6 py-4 transition hover:-translate-y-1 hover:border-white/20 hover:bg-white/10"
+              className="flex items-center gap-3 rounded-2xl border border-white/10 bg-white/5 px-6 py-4 transition hover:-translate-y-1 hover:border-white/20 hover:bg-white/10 w-full"
             >
               <FaTiktok size={24} className="text-black" />
               <span className="font-semibold">TikTok</span>
